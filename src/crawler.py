25f15cca02aa645fa5138c8e58c7259d9bf146bd
iksadnorth <iksadnorth@gmail.com>
2025-12-30 14:40:32 +0000

    필요없는 mock data 삭제 v2

--- src/crawler.py
+++ src/crawler.py
@@ -187,16 +187,10 @@ if __name__ == '__main__':
 
 class InstagramCrawler():
     def scrap_array_id(self, created_at: datetime):
-        return ['9263344374', '9263492727', '9263440283']
+        return []
     
     def scrap_article_data(self, article_id: str):
-        raw_data = [
-            {'src_pk': article_id, 'src_id': 'instagram', 'comment_id': '0', 'text': '여기까지 [LIT]임 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ', 'created_at': '2025.12.28 12:58', 'created_by': '191735343'},
-            {'src_pk': article_id, 'src_id': 'instagram', 'comment_id': '1', 'text': '왜 좋냐', 'created_at': '2025.12.28 13:02', 'created_by': '6998518866'},
-            {'src_pk': article_id, 'src_id': 'instagram', 'comment_id': '2', 'text': '존나 힙하네', 'created_at': '2025.12.28 13:02', 'created_by': '8177365530'},
-            {'src_pk': article_id, 'src_id': 'instagram', 'comment_id': '3', 'text': '좋은데 ㅋㅋ', 'created_at': '2025.12.28 13:02', 'created_by': '6904184348'},
-            {'src_pk': article_id, 'src_id': 'instagram', 'comment_id': '4', 'text': '싹 긁어냈다매 이게 존나 빡세다ㅋㅋㅋㅋ', 'created_at': '2025.12.28 13:02', 'created_by': '3856208534'},
-        ]
+        raw_data = []
         for doc in raw_data:
             if 'created_at' not in doc: continue
             normalized = normalize_created_at(doc['created_at'])
